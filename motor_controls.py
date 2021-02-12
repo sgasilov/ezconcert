98083a59614a3cb9a2b72c4ed67e5e9adde8f66c
Adam Webb <adam.webb@lightsource.ca>
2021-02-12 14:56:33 -0600

    #37 relative motion

--- motor_controls.py
+++ motor_controls.py
@@ -1,8 +1,8 @@
-from epics import PV
 import atexit
 from PyQt5.QtCore import pyqtSignal, QObject, QThread
-from PyQt5.QtWidgets import QGridLayout, QLabel, QGroupBox, QLineEdit, \
-    QPushButton, QComboBox, QFileDialog, QCheckBox, QDoubleSpinBox
+from PyQt5.QtWidgets import QGridLayout, QLabel, QGroupBox, \
+    QPushButton, QDoubleSpinBox, QFrame, \
+    QSizePolicy
 from concert.base import TransitionNotAllowed
 from message_dialog import info_message, error_message
 # Adam's Concert-EPICS interface
@@ -11,6 +11,35 @@ from edc.motor import CLSLinear, ABRS, SimMotor
 
 from concert.devices.base import abort as device_abort
 from concert.quantities import q
+
+
+class QVSeparationLine(QFrame):
+    '''
+    a vertical separation line
+    '''
+
+    def __init__(self, *args, **kwargs):
+        super(QVSeparationLine, self).__init__(*args, **kwargs)
+        self.setFixedWidth(10)
+        self.setMinimumHeight(1)
+        self.setFrameShape(QFrame.VLine)
+        self.setFrameShadow(QFrame.Sunken)
+        self.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Preferred)
+
+
+class QHSeparationLine(QFrame):
+    '''
+    a horizontal separation line
+    '''
+
+    def __init__(self, *args, **kwargs):
+        super(QHSeparationLine, self).__init__(*args, **kwargs)
+        self.setMinimumWidth(1)
+        self.setFixedHeight(10)
+        self.setFrameShape(QFrame.HLine)
+        self.setFrameShadow(QFrame.Sunken)
+        self.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Minimum)
+
 
 class MotorsControlsGroup(QGroupBox):
 
@@ -23,7 +52,8 @@ class MotorsControlsGroup(QGroupBox):
         self.shutter = None
         self.time_motor = None
         self.connect_time_motor_func()
-        self.motors = [self.hor_motor, self.vert_motor, self.CT_motor, self.shutter, self.time_motor]
+        self.motors = [self.hor_motor, self.vert_motor,
+                       self.CT_motor, self.shutter, self.time_motor]
 
         # connect buttons
         self.connect_hor_mot_button = QPushButton("horizontal stage")
@@ -41,16 +71,16 @@ class MotorsControlsGroup(QGroupBox):
         self.mot_pos_info_label.setText("Status")
         self.hor_mot_pos_label = QLabel()
         self.hor_mot_pos_label.setText("Not connected")
-        self.hor_mot_pos_entry = QLabel()
+        # self.hor_mot_pos_entry = QLabel()
         self.vert_mot_pos_label = QLabel()
         self.vert_mot_pos_label.setText("Not connected")
-        self.vert_mot_pos_entry = QLabel()
+        # self.vert_mot_pos_entry = QLabel()
         self.CT_mot_pos_label = QLabel()
         self.CT_mot_pos_label.setText("Not connected")
-        self.CT_mot_pos_entry = QLabel()
+        # self.CT_mot_pos_entry = QLabel()
         self.shutter_label = QLabel()
         self.shutter_label.setText("Not connected")
-        self.shutter_entry = QLabel()
+        # self.shutter_entry = QLabel()
 
         # position entry
         self.hor_mot_pos_move = QDoubleSpinBox()
@@ -62,6 +92,15 @@ class MotorsControlsGroup(QGroupBox):
         self.CT_mot_pos_move = QDoubleSpinBox()
         self.CT_mot_pos_move.setDecimals(3)
         self.CT_mot_pos_move.setRange(-720, 720)
+        self.hor_mot_rel_move = QDoubleSpinBox()
+        self.hor_mot_rel_move.setDecimals(3)
+        self.hor_mot_rel_move.setRange(-100, 100)
+        self.vert_mot_rel_move = QDoubleSpinBox()
+        self.vert_mot_rel_move.setDecimals(3)
+        self.vert_mot_rel_move.setRange(-100, 100)
+        self.CT_mot_rel_move = QDoubleSpinBox()
+        self.CT_mot_rel_move.setDecimals(3)
+        # self.CT_mot_rel_move.setRange(-720, 720)
 
         # Move Buttons
         self.stop_motors_button = QPushButton("STOP ALL")
@@ -79,6 +118,12 @@ class MotorsControlsGroup(QGroupBox):
         self.open_shutter_button.setEnabled(False)
         self.close_shutter_button = QPushButton("Close")
         self.close_shutter_button.setEnabled(False)
+        self.move_hor_rel_button = QPushButton("Move Relative")
+        self.move_hor_rel_button.setEnabled(False)
+        self.move_vert_rel_button = QPushButton("Move Relative")
+        self.move_vert_rel_button.setEnabled(False)
+        self.move_CT_rel_button = QPushButton("Move Relative")
+        self.move_CT_rel_button.setEnabled(False)
 
         # signals
         self.move_hor_mot_button.clicked.connect(self.hor_move_func)
@@ -89,8 +134,17 @@ class MotorsControlsGroup(QGroupBox):
         self.open_shutter_button.clicked.connect(self.open_shutter_func)
         self.close_shutter_button.clicked.connect(self.close_shutter_func)
         self.stop_motors_button.clicked.connect(self.stop_motors_func)
+        self.move_hor_rel_button.clicked.connect(self.hor_move_rel_func)
+        self.move_vert_rel_button.clicked.connect(self.vert_move_rel_func)
+        self.move_CT_rel_button.clicked.connect(self.CT_move_rel_func)
 
-        #THREADS
+        # decoration
+        self.line_vertical = QVSeparationLine()
+        self.line_vertical2 = QVSeparationLine()
+        self.line_vertical3 = QVSeparationLine()
+        self.line_vertical4 = QVSeparationLine()
+
+        # THREADS
         self.motion_hor = None
         self.motion_CT = None
         self.motion_vert = None
@@ -99,33 +153,52 @@ class MotorsControlsGroup(QGroupBox):
         self.set_layout()
 
     def set_layout(self):
-
+        '''
+        object: columns
+        stop: 0
+        CT: 2 - 4
+        vertical: 6 - 8
+        horizontal: 10 - 12
+        shutter: 14 - 16
+        vertical lines: 1, 5, 9, 13
+        '''
         layout = QGridLayout()
-        layout.addWidget(self.stop_motors_button, 0, 1, 2, 1)
-        layout.addWidget(self.connect_CT_mot_button, 0, 2, 1, 1)
-        layout.addWidget(self.connect_vert_mot_button, 0, 5, 1, 1)
-        layout.addWidget(self.connect_hor_mot_button, 0, 8, 1, 1)
-        layout.addWidget(self.connect_shutter_button, 0, 11, 1, 1)
-        layout.addWidget(self.move_CT_mot_button, 0, 3, 1, 1)
+        layout.addWidget(self.stop_motors_button, 0, 0, 3, 1)
+        layout.addWidget(self.connect_CT_mot_button, 0, 3, 1, 1)
+        layout.addWidget(self.connect_vert_mot_button, 0, 7, 1, 1)
+        layout.addWidget(self.connect_hor_mot_button, 0, 11, 1, 1)
+        layout.addWidget(self.connect_shutter_button, 0, 15, 1, 1)
+        layout.addWidget(self.move_CT_mot_button, 1, 3, 1, 1)
+        layout.addWidget(self.move_CT_rel_button, 2, 3, 1, 1)
         layout.addWidget(self.home_CT_mot_button, 0, 4, 1, 1)
         layout.addWidget(self.reset_CT_mot_button, 1, 4, 1, 1)
-        layout.addWidget(self.move_vert_mot_button, 0, 6, 1, 1)
-        layout.addWidget(self.move_hor_mot_button, 0, 9, 1, 1)
-        layout.addWidget(self.open_shutter_button, 0, 12, 1, 1)
-        layout.addWidget(self.close_shutter_button, 1, 12, 1, 1)
+        layout.addWidget(self.move_vert_mot_button, 1, 7, 1, 1)
+        layout.addWidget(self.move_vert_rel_button, 2, 7, 1, 1)
+        layout.addWidget(self.move_hor_mot_button, 1, 11, 1, 1)
+        layout.addWidget(self.move_hor_rel_button, 2, 11, 1, 1)
+        layout.addWidget(self.open_shutter_button, 1, 15, 1, 1)
+        layout.addWidget(self.close_shutter_button, 2, 15, 1, 1)
         # layout.addWidget(self.mot_pos_info_label, 1, 1)
-        layout.addWidget(self.CT_mot_pos_label, 1, 2)
-        layout.addWidget(self.CT_mot_pos_entry, 1, 3)
-        layout.addWidget(self.vert_mot_pos_label, 1, 5)
-        layout.addWidget(self.vert_mot_pos_entry, 1, 6)
-        layout.addWidget(self.hor_mot_pos_label, 1, 8)
-        layout.addWidget(self.hor_mot_pos_entry, 1, 9)
-        layout.addWidget(self.shutter_label, 1, 11)
+        layout.addWidget(self.CT_mot_pos_label, 0, 2)
+        # layout.addWidget(self.CT_mot_pos_entry, 1, 1)
+        layout.addWidget(self.vert_mot_pos_label, 0, 6)
+        # layout.addWidget(self.vert_mot_pos_entry, 1, 4)
+        layout.addWidget(self.hor_mot_pos_label, 0, 10)
+        # layout.addWidget(self.hor_mot_pos_entry, 1, 7)
+        layout.addWidget(self.shutter_label, 0, 14)
         # layout.addWidget(self.shutter_entry, 1, 12)
 
-        layout.addWidget(self.hor_mot_pos_move, 1, 9)
+        layout.addWidget(self.hor_mot_pos_move, 1, 10)
         layout.addWidget(self.vert_mot_pos_move, 1, 6)
-        layout.addWidget(self.CT_mot_pos_move, 1, 3)
+        layout.addWidget(self.CT_mot_pos_move, 1, 2)
+        layout.addWidget(self.hor_mot_rel_move, 2, 10)
+        layout.addWidget(self.vert_mot_rel_move, 2, 6)
+        layout.addWidget(self.CT_mot_rel_move, 2, 2)
+
+        layout.addWidget(self.line_vertical, 0, 1, 3, 1)
+        layout.addWidget(self.line_vertical2, 0, 5, 3, 1)
+        layout.addWidget(self.line_vertical3, 0, 9, 3, 1)
+        layout.addWidget(self.line_vertical4, 0, 13, 3, 1)
 
         self.setLayout(layout)
 
@@ -138,6 +211,7 @@ class MotorsControlsGroup(QGroupBox):
             self.hor_mot_pos_label.setText("Position [mm]")
             self.connect_hor_mot_button.setEnabled(False)
             self.move_hor_mot_button.setEnabled(True)
+            self.move_hor_rel_button.setEnabled(True)
             self.hor_mot_monitor = EpicsMonitorFloat(self.hor_motor.RBV)
             self.hor_mot_monitor.i0_state_changed_signal.connect(
                 self.hor_mot_pos_label.setText)
@@ -152,6 +226,7 @@ class MotorsControlsGroup(QGroupBox):
             self.vert_mot_pos_label.setText("Position [mm]")
             self.connect_vert_mot_button.setEnabled(False)
             self.move_vert_mot_button.setEnabled(True)
+            self.move_vert_rel_button.setEnabled(True)
             self.vert_mot_monitor = EpicsMonitorFloat(self.vert_motor.RBV)
             self.vert_mot_monitor.i0_state_changed_signal.connect(
                 self.vert_mot_pos_label.setText)
@@ -166,6 +241,7 @@ class MotorsControlsGroup(QGroupBox):
             self.CT_mot_pos_label.setText("Position [deg]")
             self.connect_CT_mot_button.setEnabled(False)
             self.move_CT_mot_button.setEnabled(True)
+            self.move_CT_rel_button.setEnabled(True)
             self.home_CT_mot_button.setEnabled(True)
             self.reset_CT_mot_button.setEnabled(True)
             self.CT_mot_monitor = EpicsMonitorFloat(self.CT_motor.RBV)
@@ -193,7 +269,6 @@ class MotorsControlsGroup(QGroupBox):
             self.time_motor = SimMotor(1.0*q.mm)
         except:
             error_message("Can not connect to timer")
-
 
     def open_shutter_func(self):
         if self.shutter is None:
@@ -236,6 +311,15 @@ class MotorsControlsGroup(QGroupBox):
             self.motion_CT = MotionThread(self.CT_motor, self.CT_mot_pos_move)
             self.motion_CT.start()
 
+    def CT_move_rel_func(self):
+        '''Move the stage a relative amount'''
+        if self.CT_motor is None:
+            return
+        else:
+            self.CT_motor.stepvelocity = 5.0 * q.deg/q.sec
+            self.motion_CT = MotionThread(
+                self.CT_motor, self.CT_mot_pos_move, self.CT_mot_rel_move)
+            self.motion_CT.start()
 
     def CT_reset_func(self):
         '''Reset the stage and move to home'''
@@ -254,12 +338,18 @@ class MotorsControlsGroup(QGroupBox):
             self.motion_hor = MotionThread(self.hor_motor, self.hor_mot_pos_move)
             self.motion_hor.start()
 
+    def hor_move_rel_func(self):
+        pass
+
     def vert_move_func(self):
         if self.vert_motor is None:
             return
         else:
             self.motion_vert = MotionThread(self.vert_motor, self.vert_mot_pos_move)
             self.motion_vert.start()
+
+    def vert_move_rel_func(self):
+        pass
 
     def stop_motors_func(self):
         # pyqt threads
@@ -279,6 +369,7 @@ class MotorsControlsGroup(QGroupBox):
         #     device_abort([self.CT_motor])
         # if self.shutter is not None:
         #     device_abort([self.shutter])
+
 
 class EpicsMonitorFloat(QObject):
     i0_state_changed_signal = pyqtSignal(str)
@@ -328,12 +419,13 @@ class EpicsMonitorFIS(QObject):
 
 class MotionThread(QThread):
 
-    def __init__(self, motor, position):
+    def __init__(self, motor, position, rel_position=None):
         super(MotionThread, self).__init__()
         self.motor = motor
         self.thread_running = True
         atexit.register(self.stop)
         self.position = position
+        self.rel_position = rel_position
 
     def stop(self):
         self.thread_running = False
@@ -342,7 +434,11 @@ class MotionThread(QThread):
     def run(self):  # .start() calls this function
         while self.thread_running:
             try:
-                self.motor.position = self.position.value() * self.motor.UNITS
+                if self.rel_position is None:
+                    rel_pos_value = 0.0
+                else:
+                    rel_pos_value = self.rel_position.value()
+                self.motor.position = rel_pos_value + self.position.value() * self.motor.UNITS
                 self.thread_running = False
             except TransitionNotAllowed:
                 error_message("Stage is moving. Wait until motion has stopped.")
@@ -405,4 +501,3 @@ class ResetThread(QThread):
             self.motor.abort()
         except:
             pass
-
